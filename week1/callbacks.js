9293b1a31768edc07688e5389d50645806767540
KlimentovaJ <julie.klimentova@wunderman.com>
2018-10-12 11:50:55 +0200

    Test for null, undefine, '' added

--- week1/callbacks.js
+++ week1/callbacks.js
@@ -8,23 +8,26 @@ const BASE_URL = 'http://swapi.co/api/';
 
 function callSwapi(specificUrl, callback) {
   const SPECIFICURL = specificUrl;
+  const err = "Something bad happened.";
   request.get(`${BASE_URL}${SPECIFICURL}`, callback);
 }
 
 callSwapi('people/1', (err, response, body) => {
-  const result = JSON.parse(body);
-  const vehiclesURLS = result.vehicles;
+  let vehiclesURLS;
+  if (body !== null && body !== undefined && body !== "") {
+    vehiclesURLS = JSON.parse(body).vehicles;
+  }
   const vehiclesSpecific = vehiclesURLS.map((el) => {
     return el.slice(21);
   });
 
- vehiclesSpecific.map((spec) => {
-   callSwapi(spec, (err, response, body) => {
-     let vehicleResult = JSON.parse(body);
-     let vehicleName = vehicleResult.name;
-     console.log(vehicleName);
-   })
- })
+  vehiclesSpecific.map((spec) => {
+    callSwapi(spec, (err, response, body) => {
+      let vehicleResult = JSON.parse(body);
+      let vehicleName = vehicleResult.name;
+      console.log(vehicleName);
+    })
+  })
 });
 
 
